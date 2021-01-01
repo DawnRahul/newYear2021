b6b06a6a6b8a3966062e64f6d0b377ac3f1b509c
Rahul Kumar <45137578+DawnRahul@users.noreply.github.com>
2021-01-01 15:40:06 +0530

    Final Update 2.0
    Added option for language change between english and hindi for the wish.

--- wish-you.js
+++ wish-you.js
@@ -1,9 +1,23 @@
 
 let targetSendButton = document.querySelector(".send-button");
 let sharingDialogBox = document.querySelector(".share-dialog-box-container");
+let targetHindiButton = document.querySelector(".lang-hindi");
+let targetEnglishButton = document.querySelector(".lang-english");
 
 
+// Language buttons for changing language of the message
 
+targetHindiButton.addEventListener("click", function(){
+    document.querySelector(".message-english").style.display = "none";
+    document.querySelector(".message-hindi").style.display = "block";
+});
+
+targetEnglishButton.addEventListener("click", function(){
+    document.querySelector(".message-hindi").style.display = "none";
+    document.querySelector(".message-english").style.display = "block";
+});
+
+// Play sound on page visit after 10 milliseconds
     setTimeout(() => {
         let mySound = new Audio("sound/sound-hey.mp3");
         setInterval(() => {
@@ -27,4 +41,7 @@ let closeDialogBox = document.querySelector(".close-dialog");
 
 closeDialogBox.addEventListener("click", function(){
     sharingDialogBox.style.display = "none";
-})
+});
+
+
+
